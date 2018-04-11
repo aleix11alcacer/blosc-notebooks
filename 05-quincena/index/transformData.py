6a0d25c979df9b88c322cedbb0676bd2d5552289
Aleix Alcacer Sales <aleix11alcacer@MacBook-Pro-de-Aleix.local>
2018-04-11 12:25:43 +0200

    Obtener indices

---
+++ 05-quincena/index/transformData.py
@@ -0,0 +1,38 @@
+'''
+Implements functions to use tData library.
+'''
+
+import numpy as np
+from tData import ffi, lib
+
+
+def tData(src, sub_shp, inverse=False):
+    """
+    Execute tData function of tData library.
+
+    Parameters
+    ----------
+    src : np.array
+        Data to transform.
+    sub_shp: int[] or tuple
+        Data partition shape.
+    inverse: bool, optional
+
+    Returns
+    -------
+    dest: np.array
+        Data transformed.
+    """
+
+    dest = np.zeros(src.size, dtype=src.dtype).reshape(src.shape)
+
+    typesize = src.dtype.itemsize
+    shape = src.shape
+    dimension = len(shape)
+
+    src2 = ffi.from_buffer(src)
+    dest2 = ffi.from_buffer(dest)
+
+    lib.tData(src2, dest2, typesize, sub_shp, shape, dimension, inverse)
+
+    return dest
